8a26f20c47967530281d9781e0ad62536c6869f0
Cole Hanson <chanson22@huskers.unl.edu>
2022-08-25 00:00:21 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Cole Hanson
+ *         chanson22@huskers.unl.edu
+ * Date: 2022/8/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cole Hanson, Computer Engineering\n");
+
+  return 0;
+}
